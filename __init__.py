1b74d6046d95b373c69960cd54f77d45a480c11e
Alexey-T <support@uvviewsoft.com>
2016-04-09 10:12:49 +0300

    dialog

--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
 import os
-import re
 import tempfile
 import zipfile
 import shutil
@@ -33,14 +32,67 @@ def do_copy_dir(src, dest):
         print('Directory not copied. Error: %s' % e)
 
 
+def do_install_from_dir(dir_from):        
+    fn_inf = os.path.join(dir_from, 'install.inf')
+    if not os.path.isfile(fn_inf):
+        msg_box('Cannot find install.inf in zip', MB_OK+MB_ICONERROR)
+        return
+        
+    subdir = ini_read(fn_inf, 'info', 'subdir', '')
+    typ = ini_read(fn_inf, 'info', 'type', '')
+    if not subdir:
+        msg_box('Cannot find subdir-value in install.inf', MB_OK+MB_ICONERROR)
+        return
+    if typ!='cudatext-plugin':
+        msg_box('Cannot install this addon type: '+typ, MB_OK+MB_ICONERROR)
+        return
+        
+    dir_to = os.path.join(app_path(APP_DIR_PY), subdir)
+    print('Installing to: '+dir_to)
+        
+    dir_trash = os.path.join(app_path(APP_DIR_PY), '__trash')
+    if not os.path.isdir(dir_trash):
+        os.mkdir(dir_trash)
+    if os.path.isdir(dir_to):
+        dir_del = os.path.join(dir_trash, subdir)
+        while True:
+            if not os.path.isdir(dir_del):
+                os.rename(dir_to, dir_del)
+                break
+            dir_del += '_'
+        
+    do_copy_dir(dir_from, dir_to)
+    ed.cmd(cudatext_cmd.cmd_RescanPythonPluginsInfFiles)
+    msg_box('Plugin installed to:\n'+dir_to, MB_OK+MB_ICONINFO)
+    
+
 class Command:
     def run(self):
-        url = dlg_input('Github repo URL:', 'https://github.com/kvichans/cuda_find_in_files')
+        fn_hist = os.path.join(app_path(APP_DIR_SETTINGS), 'cuda_install_from_github.ini')
+        list_hist = ['https://github.com/kvichans/cuda_find_in_files']
+        if os.path.isfile(fn_hist):
+            list_hist = open(fn_hist).read().splitlines()
+    
+        c1 = chr(1)
+        id_edit = 1
+        id_ok = 2
+        id_cancel = 3
+        res = dlg_custom('Install from Github', 506, 90, '\n'.join([]
+          + [c1.join(['type=label', 'cap=&Github repo URL', 'pos=6,6,400,0'])]
+          + [c1.join(['type=combo', 'items='+'\t'.join(list_hist), 'pos=6,26,500,0'])]
+          + [c1.join(['type=button', 'cap=OK', 'pos=296,60,396,0', 'props=1'])]
+          + [c1.join(['type=button', 'cap=Cancel', 'pos=400,60,500,0'])]
+          ))
+        if not res: return
+        btn, text = res
+        if btn!=id_ok: return
+        text = text.splitlines()
+        url = text[id_edit]
         if not url: return
-        url += '/zipball/master'
+        
         fn = os.path.join(tempfile.gettempdir(), 'cudatext_addon.zip')
         msg_status('Downloading zip...')
-        get_url(url, fn)
+        get_url(url+'/zipball/master', fn)
         msg_status('')
         if not os.path.isfile(fn):
             msg_status('Cannot download URL')
@@ -64,36 +116,10 @@ class Command:
         
         #1st subdir is what to copy    
         dir_from = os.path.join(dir, os.listdir(dir)[0])
-        
-        fn_inf = os.path.join(dir_from, 'install.inf')
-        if not os.path.isfile(fn_inf):
-            msg_box('Cannot find install.inf in zip', MB_OK+MB_ICONERROR)
-            return
-        
-        subdir = ini_read(fn_inf, 'info', 'subdir', '')
-        typ = ini_read(fn_inf, 'info', 'type', '')
-        if not subdir:
-            msg_box('Cannot find subdir-value in install.inf', MB_OK+MB_ICONERROR)
-            return
-        if typ!='cudatext-plugin':
-            msg_box('Cannot install this addon type: '+typ, MB_OK+MB_ICONERROR)
-            return
-        
-        dir_to = os.path.join(app_path(APP_DIR_PY), subdir)
-        print('Installing to: '+dir_to)
-        
-        dir_trash = os.path.join(app_path(APP_DIR_PY), '__trash')
-        if not os.path.isdir(dir_trash):
-            os.mkdir(dir_trash)
-        if os.path.isdir(dir_to):
-            dir_del = os.path.join(dir_trash, subdir)
-            while True:
-                if not os.path.isdir(dir_del):
-                    os.rename(dir_to, dir_del)
-                    break
-                dir_del += '_'
-        
-        do_copy_dir(dir_from, dir_to)
-        ed.cmd(cudatext_cmd.cmd_RescanPythonPluginsInfFiles)
-        msg_box('Plugin installed to:\n'+dir_to, MB_OK+MB_ICONINFO)
-        
+        do_install_from_dir(dir_from)
+
+        #save history        
+        if not url in list_hist:
+            list_hist = [url]+list_hist
+            with open(fn_hist, 'w') as f:
+                f.write('\n'.join(list_hist))
